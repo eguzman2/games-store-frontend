feaa554442c5fcf4798946c89ea851f17bb8d8c1
eguzman2 <edgantonio00@gmail.com>
2020-11-08 00:06:36 -0600

    [REF] edit server url

--- js/urls.js
+++ js/urls.js
@@ -1,4 +1,5 @@
-var url = "http://localhost:5000";
+// var url = "http://localhost:5000";
+var url = "https://gamesstore-backend.herokuapp.com/";
 var url_login = url + "/user/login";
 var url_recover_password = url + "/user/forgot_password";
 var url_register = url + "/user/register";
